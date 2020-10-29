5bcfd4c6b4de046a4367fe1c98a8bc0f9c573e5a
Delia Constantino <dconstan92@gmail.com>
2020-10-29 18:10:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,13 @@
 def prime(num)
 #must be positive
 #must be greter than 1
+#cannot be divisible by 2, 3, 5, 7
 
 if num < 2
   return false
+end
+
+if num > 4
+  num % 2 == 0 || num % 3 == 0
+  return false
+end
