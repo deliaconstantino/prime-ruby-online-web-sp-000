c30436e15860e1934763ca9940e6e0fc920a7d9c
Delia Constantino <dconstan92@gmail.com>
2020-10-29 17:47:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,30 @@
 def prime?(num)
-  if num <= 0
-    return false
-  elsif num % num == 0
-    return true
-  else
+  if num <= 3
+    return num > 1
+  elsif num % 2 == 0 || num % 3 == 0
     return false
   end
+
+  i = 5
+  while i** <= num
+    if num % i == 0 || num % (i + 2) == 0
+      return false
+    end
+    i += 6
+  end
+return true
 end
+
+
+# def isprime(n: int) -> bool:
+#     """Primality test using 6k+-1 optimization."""
+#     if n <= 3:
+#         return n > 1
+#     elif n % 2 == 0 or n % 3 == 0:
+#         return False
+#     i: int = 5
+#     while i * i <= n:
+#         if n % i == 0 or n % (i + 2) == 0:
+#             return False
+#         i += 6
+#     return True
